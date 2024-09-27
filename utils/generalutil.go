b11d3c43c3fa15f49a68147ed5f8843cecae41bb
afauzansw <afsw.fauzan33142@gmail.com>
2024-09-28 03:50:33 +0700

    create date format util

--- utils/generalutil.go
+++ utils/generalutil.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"log"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func GenerateSlug(input string) string {
@@ -16,4 +18,17 @@ func GenerateSlug(input string) string {
 	slug = strings.Trim(slug, "-")
 
 	return slug
+}
+
+func DateFormat(input time.Time) string {
+	zone, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	localTime := input.In(zone)
+
+	formatedTime := localTime.Format("01 January 2024 15:04:05")
+
+	return formatedTime
 }
