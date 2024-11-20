1b9817ae6ac5280aa6ce3750985ed3af17eb5797
afauzansw <afsw.fauzan33142@gmail.com>
2024-11-20 16:32:41 +0700

    parsing data to dashboard

--- controllers/home/homecontroller.go
+++ controllers/home/homecontroller.go
@@ -1,18 +1,27 @@
 package homecontroller
 
 import (
+	postmodel "go-web-crud/models/post"
 	"net/http"
 	"text/template"
 )
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
+	posts := postmodel.GetAll()
+	data := map[string]any{
+		"posts": posts,
+		"totalPostToday": 3,
+		"totalPost": 15,
+		"totalPublisher": 5,
+	}
+	
 	files, err := template.ParseFiles("views/pages/home/home.html")
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = files.Execute(w, nil)
+	err = files.Execute(w, data)
 	if err != nil {
 		panic(err)
 	}
